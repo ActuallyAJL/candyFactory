af5d4b152e58d973223a1bf4ece215fa1aab8589
DrLeisure <zero3260@hotmail.com>
2022-02-17 10:26:11 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const buyChocolate = () => {
+    const chocolateBar = {
+        type: "milk chocolate"
+    }
+    return chocolateBar
+}
+const addFlavoring = (chocolateObject) => {
+    chocolateObject.flavor = "Mint"
+    return chocolateObject
+}
+const makeBarkMixture = (chocolateObject) => {
+    if (chocolateObject.flavor === "Mint") {
+        chocolateObject.mixed = true
+    } else {
+        chocolateObject.mixed = false
+    }
+    return chocolateObject
+}
+const bakeCandy = (chocolateObject) => {
+    if (chocolateObject.mixed === true) {
+        chocolateObject.baked = true
+    } else {
+        chocolateObject.baked = false
+    }
+    return chocolateObject
+}
+const breakBark = (chocolateObject) => {
+    if (chocolateObject.baked === true) {
+        const brokenChocolate = ["Mint Chocolate Bark Piece" , "Mint Chocolate Bark Piece" , "Mint Chocolate Bark Piece" , "Mint Chocolate Bark Piece" , "Mint Chocolate Bark Piece" , "Mint Chocolate Bark Piece"]
+        return brokenChocolate
+    }
+}
+
+const cookCandy = () => {
+    let cookedCandy = {}
+    cookedCandy = buyChocolate()
+    addFlavoring(cookedCandy)
+    makeBarkMixture(cookedCandy)
+    bakeCandy(cookedCandy)
+    return breakBark(cookedCandy)
+}
+
+console.log(cookCandy())
